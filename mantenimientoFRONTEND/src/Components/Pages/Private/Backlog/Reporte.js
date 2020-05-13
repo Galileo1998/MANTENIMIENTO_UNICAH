bb89072f46a07d6e421844d2885d67ec0d343a6c
Galileo <galileoga63@gmail.com>
2020-05-13 15:25:54 -0600

    Imágenes

--- mantenimientoFRONTEND/src/Components/Pages/Private/Backlog/Reporte.js
+++ mantenimientoFRONTEND/src/Components/Pages/Private/Backlog/Reporte.js
@@ -4,31 +4,65 @@ import Campo from '../../../Common/Campo/Campo';
 import { paxios } from '../../../../Utilities';
 import './detalleOrden.css';
 import TextArea from '../../../Common/TextArea/TextArea';
+import {Page, Text, View, Document, StyleSheet, PDFViewer, Image} from '@react-pdf/renderer';
 
+const styles = StyleSheet.create({
+    page: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+    },
+    section: {
+        margin: 10,
+        padding: 10,
+        flexGrow: 1
+    },
+    viewer: {
+        width: '100%',
+        height: '100%',
+        marginTop: '65px'
+    },
+    h1: {
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    h2: {
+        textAlign: 'center',
+        fontSize: 14,
+        fontWeight: 'normal',
+        fontStyle: 'italic'
+    },
+    h3: {
+        textAlign: 'center',
+        fontSize: 12
+    },
+    image: {
+        marginVertical: 15,
+        marginHorizontal: 100,
+        width: '45px',
+        height: '60px',
+        alignSelf: 'center'
+    }
+});
 
 
 export default class Reporte extends Component {
-    constructor(){
-        super();
-        this.state = {
-          things:[],
-          isLoading: false,
-          error: false,
-        }
-
-      }
-
-
   render() {
-    console.log(this.state);
     return (
-        <section className="inicio" >
-            <center>
-                <h1>Reporte de mantenimiento</h1>
-                <section className="main fix640 " >
-                </section>
-            </center>
-        </section>
+
+        <PDFViewer style={styles.viewer}>
+            <Document>
+            <Page size="Letter" style={styles.page}>
+                <View style={styles.section}>
+                    <Text></Text>
+                    <Image style={styles.image} src='/LOGO_2C1.png'/>
+                    <Text style={styles.h1}>UNIVERSIDAD CATÓLICA DE HONDURAS</Text>
+                    <Text style={styles.h2}>"Nuestra Señora Reina de la Paz"</Text>
+                    <Text style={styles.h3}>Reporte de mantenimiento</Text>
+                </View>    
+            </Page>
+            </Document>
+        </PDFViewer>
     );
   }
 }
