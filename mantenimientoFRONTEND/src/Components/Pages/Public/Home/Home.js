1aa199f90a4ac0ae46e3bfbc1177bc18860e1c6a
Galileo <galileoga63@gmail.com>
2020-05-13 17:48:18 -0600

    Fondo

--- mantenimientoFRONTEND/src/Components/Pages/Public/Home/Home.js
+++ mantenimientoFRONTEND/src/Components/Pages/Public/Home/Home.js
@@ -7,15 +7,11 @@ export default class Home extends Component{
   render() {
     return (
         <div className="home">
-          <div>
-          <h1>MANTENIMIENTO UNICAH</h1>
-          </div>
-
+          <h1><img src="/LOGO_2C1.png" style={{width: "50px", height: "60px"}}/>&nbsp;&nbsp;&nbsp;&nbsp;<span>MANTENIMIENTO</span>&nbsp;<span>UNICAH</span></h1>
             <div>&nbsp;</div>
             <div className="btnout">
             {(this.props.auth.logged) ? (<center><div className="half"><Button customClass="fond" onClick={(e) => { this.props.setUnAuth(false)}}><IoIosLogOut/>&nbsp;Cerrar Sesión</Button></div></center>):null}
             </div>
-            
         </div>
     );
   }
